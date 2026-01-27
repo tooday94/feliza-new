649b3956f1404d4c73ee5f78e3f7c40d0e2154a7
fridrax <61906771+fridrax@users.noreply.github.com>
2026-01-27 11:28:10 +0500

    Simplify build configuration by removing manual chunks
    Removed custom Rollup manual chunking configuration from build options.

--- vite.config.js
+++ vite.config.js
@@ -9,33 +9,10 @@ export default defineConfig({
     port: 3000,
   },
   build: {
+    // Оставляем минификацию для скорости
     minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-        
-            if (
-              id.includes('react') || 
-              id.includes('react-dom') || 
-              id.includes('react-router-dom') || 
-              id.includes('antd') || 
-              id.includes('@ant-design')
-            ) {
-              return 'vendor';
-            }
-            
-            // Только иконки можно безопасно отделить, они ни от чего не зависят
-            if (id.includes('react-icons')) {
-              return 'icons';
-            }
-
-            // Остальные мелкие библиотеки
-            return 'libs';
-          }
-        },
-      },
-    },
+    // Увеличиваем лимит, чтобы не было предупреждений
     chunkSizeWarningLimit: 1600,
+ 
   },
 });
