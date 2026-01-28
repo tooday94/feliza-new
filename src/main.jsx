26d14785f11e2c34ab592510242ae457102e883b
fridrax <61906771+fridrax@users.noreply.github.com>
2026-01-28 22:21:27 +0500

    Wrap application in HelmetProvider for SEO support

--- src/main.jsx
+++ src/main.jsx
@@ -7,24 +7,29 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./i18n";
 import { ConfigProvider } from "antd";
 
+import { HelmetProvider } from "react-helmet-async";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={new QueryClient()}>
-      <ConfigProvider
-        theme={{
-          token: { colorPrimary: "#0d0d0d" },
-          components: {
-            Tabs: {
-              itemColor: "#bbb",
-              itemActiveColor: "#0d0d0d",
+
+    <HelmetProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <ConfigProvider
+          theme={{
+            token: { colorPrimary: "#0d0d0d" },
+            components: {
+              Tabs: {
+                itemColor: "#bbb",
+                itemActiveColor: "#0d0d0d",
+              },
             },
-          },
-        }}
-      >
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ConfigProvider>
-    </QueryClientProvider>
+          }}
+        >
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ConfigProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   </StrictMode>
 );
